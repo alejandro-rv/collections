a8f89ce66c53099923f854456aacc47a78e5652f
Alejandro Ramírez <alejandro.ramirez.vargas@outlook.es>
2021-02-20 14:30:33 +0100

    Added relationship between collections and collectables

--- app/Collectable.php
+++ app/Collectable.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collectable extends Model
 {
-    //
+    /**
+     * Get the collection that owns the collectable.
+     */
+    public function collection()
+    {
+        return $this->belongsTo('App\Collection');
+    }
 }
--- app/Collection.php
+++ app/Collection.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collection extends Model
 {
-    //
+    /**
+     * Get the collectables for the collection.
+     */
+    public function collectables()
+    {
+        return $this->hasMany('App\Collectable');
+    }
 }
